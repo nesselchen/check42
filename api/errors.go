4c1da2d1d44408c2d40c5d21973dbb407d972b1c
nessel <w.2nert@gmail.com>
2024-05-30 19:29:27 +0200

    Extract error codes

---
+++ api/errors.go
@@ -0,0 +1,49 @@
+package api
+
+import (
+	"check42/api/router"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func fail(w http.ResponseWriter, status int, msg string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	io.WriteString(w, msg)
+}
+
+var statusOK = router.HttpStatus{
+	Code: http.StatusOK,
+	Err:  nil,
+}
+
+var statusCreated = router.HttpStatus{
+	Code: http.StatusCreated,
+	Err:  nil,
+}
+
+var internalError = router.HttpStatus{
+	Code: http.StatusInternalServerError,
+	Err:  errors.New("internal error"),
+}
+
+func internalErrorCause(cause error) router.HttpStatus {
+	fmt.Println("Internal error:", cause)
+	return internalError
+}
+
+func badRequestCause(cause error) router.HttpStatus {
+	return router.HttpStatus{
+		Code: http.StatusBadRequest,
+		Err:  cause,
+	}
+}
+
+func notFound(id int64) router.HttpStatus {
+	return router.HttpStatus{
+		Code: http.StatusNotFound,
+		Err:  fmt.Errorf("no item %d found", id),
+	}
+}
